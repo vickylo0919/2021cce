2fb1050559ead8a262afb8ab236fb04e8cd50273
羅璟儀 <91250444+vickylo0919@users.noreply.github.com>
2021-10-28 17:43:18 +0800

    Add files via upload

---
+++ week07/week07-4.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i;///1973發明C語言1989年 ANSI C標準
+    for (i=0;i<=3;i++){
+        printf("課本的寫法\n");
+    }
+    ///1999年/1998年的C/C++的規定
+    for (int i=0;i<3;i++){
+        printf("小葉老師教的寫法\n");
+    }
+}
